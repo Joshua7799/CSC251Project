20eec4ed5fd54933a45860245e3b5c95e3b2c4e4
Joshua <jt7152004@gmail.com>
2023-10-30 09:06:41 -0400

    Update Policy.java
    moved fields and methods to the policy holder class

--- Policy.java
+++ Policy.java
@@ -4,22 +4,10 @@ public class Policy
    private int policyNumber; 
    
    private String providerName;
-
-   private String firstName;
-
-   private String lastName;
-
-   private int age;
-
-   private String smokingStatus;
-
-   private double height;
-
-   private double weight;
-
-   private double bmi;
-   
+      
    public double price = 500;
+   
+   private PolicyHolder policyHolder;
    
    /**
    
@@ -34,25 +22,12 @@ public class Policy
       policyNumber = 0;
 
       providerName = "Unentered";
-
-      firstName = "Unentered";
-
-      lastName = "Unentered";
-
-      age = 0;
-
-      smokingStatus = "Unentered";
-
-      height = 0.0;
-
-      weight = 0.0;
       
-      calculateBMI();
+      policyHolder = new PolicyHolder();
       
       calculatePrice();
       
       
-
    }
    
    
@@ -78,20 +53,8 @@ public class Policy
 
       providerName = provider;
 
-      firstName = nameFirst;
-
-      lastName = nameLast;
-
-      age = ageNumber;
-
-      smokingStatus = smoker;
-
-      height = heightInches;
-
-      weight = weightLbs;
-      
-      calculateBMI();
-            
+      policyHolder = new PolicyHolder(nameFirst, nameLast, ageNumber, smoker, heightInches, weightLbs);
+                  
       calculatePrice();
      
    }
@@ -141,179 +104,27 @@ public class Policy
    }
    
    /**
-   method to set first name
-   @param nameFirst first name
-   */
-
-   public void setFirstName(String nameFirst){
-
-      firstName = nameFirst;
-
-   }
-
-   /**
-   method to return first name
-   @return the first name
-   */
-
-   public String getFirstName(){
-
-      return firstName;
-
-   }
-
-   /**
-   method to set the last name
-   @param nameLast the last name
-   */
-
-   public void setLastName(String nameLast){
-
-      lastName = nameLast;
-
-   }
-   /**
-   method to get last name
-   @return the last name
-   */
-
-   public String getLastName(){
-
-      return lastName;
-
-   }
-   
-   /**
-   method to set age
-   @param ageNumber the age in years
-   */
-
-   public void setAge(int ageNumber){
-
-      age = ageNumber;
-
-   }
-   
-   /**
-   method to return age
-   @return the age
-   */
-
-   public int getAge(){
-
-      return age;
-
-   }
-   
-   /**
-   method to set the smoking status 
-   @param smoker if the person is a smoker or not
-   */
-
-   public void setSmokingStatus(String smoker){
-
-      smokingStatus = smoker;
-
-   }
-   
-   /**
-   method to return if the person is a smoker or not
-   @return the smoking status of the person
-   */
-
-   public String getSmokingStatus(){
-
-      return smokingStatus;
-
-   }
-   
-   /**
-   method to set the height of the person
-   @param heightInches the height of the person in inches
-
-   */
-
-   public void setHeight(int heightInches){
-
-      height = heightInches;
-   
-   }
-   
-   /**
-   method to return the height of the person
-   @return the height of the person in pounds
-   */
-
-   public double getHeight(){
-
-      return height;
-
-   }
-   
-   /**
-   method to set weight
-   @param weight the weight in pounds
-   */
-
-   public void setWeight(int weightLbs){
-
-      weight = weightLbs;
-
-   }
-   
-   /**
-   method to return weight
-   @return the weight in pounds
-   */
-
-   public double getWeight(){
-
-      return weight;
-
-   }
-   
-   /**
-   method to calculate bmi based on height and weight provided
-   */
-
-   private void calculateBMI(){
-
-      bmi = (weight* 703)/ (height * height);
-      
-   }
-   
-   /**
-   method to return bmi
-   @return the bmi
-   */
-
-   public double getBMI(){
-      
-      return bmi;
-   
-   }
-   /**
    calculates the price of the policy based on age, bmi, and smoking status
    */
 
    private void calculatePrice(){
    
    
-      if(age > 50){
+      if(policyHolder.getAge() > 50){
 
          price += price + 70;
 
       }
 
-      if(smokingStatus.equals("smoker")){
+      if(policyHolder.getSmokingStatus().equals("smoker")){
 
          price += price + 75;
 
       }
 
-      if(bmi > 35){
+      if(policyHolder.getBMI() > 35){
 
-         price += ((bmi - 35) * 20);
+         price += ((policyHolder.getBMI() - 35) * 20);
 
       }
       
